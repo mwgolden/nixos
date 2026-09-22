c1527939b6f4540b08a7dc9374cad119262566a7
Matthew <matthew@wgolden.com>
2026-09-22 06:48:19 -0500

    add static ips to nix machines

--- installer/configuration.nix
+++ installer/configuration.nix
@@ -10,11 +10,22 @@
       git
   ];
 
+  networking.interfaces.ens18.ipv4.addresses = [{
+    address = "192.168.1.10";
+    prefixLength = 24;
+  }];
+
+  networking.defaultGateway = "192.168.1.1";
+
+  networking.nameservers = [
+    "192.168.1.1"
+  ];
+  
   services.openssh.enable = true;
 
   systemd.services.sshd.wantedBy = pkgs.lib.mkForce [ "multi-user.target" ];
 
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 <public-key> nixos-installer"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMSE+JTfGnTV5FuH4nj1+9daykK+J7D+9zq0vxkpmm6H nixos-installer"
   ];
 }
